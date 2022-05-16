927754050e368c189460386618d4a82591db92dd
naufal55 <moehhammadnofal@gmail.com>
2022-05-17 02:32:50 +0700

    inisialisasi project

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Container from './Container';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { NativeBaseProvider, extendTheme } from "native-base";
+
+// Import font with Expo
+import {
+  BalsamiqSans_400Regular,
+  BalsamiqSans_400Regular_Italic,
+  useFonts,
+} from '@expo-google-fonts/balsamiq-sans'
+import AppLoading from "expo-app-loading";
+
+const App = ()=> {
+  let [fonstLoaded] = useFonts({
+    BalsamiqSans_400Regular,
+    BalsamiqSans_400Regular_Italic
+  })
+
+  // Setup Font
+  const fontConfig = {
+    BalsamiqSans: {
+      400: {
+        normal: 'BalsamiqSans_400Regular',
+        italic: 'BalsamiqSans_400Regular_Italic'
+      }
+    }
+  }
+
+  // Setup Theme
+  const customeColor =  {
+    primary: {
+      50: '#dbf4ff',
+      100: '#addbff',
+      200: '#7cc2ff',
+      300: '#4aa9ff',
+      400: '#1a91ff',
+      500: '#0077e6',
+      600: '#005db4',
+      700: '#004282',
+      800: '#002851',
+      900: '#000e21',
+    }
+  }
+
+  // Configuration Native Base Custom Theme
+  const theme = extendTheme({
+    colors: customeColor,
+    fontConfig,
+    font: {
+      heading: "BalsamiqSans",
+      body: "BalsamiqSans",
+      mono: "BalsamiqSans",
+    },
+    config: {
+      initialColorMode: 'dark'
+    }
+  })
+
+  if(!fonstLoaded){
+    return <AppLoading />
+  } else {
+    return (
+      // Setup Provider
+      <NativeBaseProvider theme={theme}>
+        <Container />
+      </NativeBaseProvider>
+    );
+  }
 }
+export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
